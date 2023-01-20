1fe85a1899b013b6dd3ccc81a186126c1a2c8270
Andrew <asundquistdavis@gmail.com>
2023-01-19 19:48:28 -0600

    add react component test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-    return (
-        <div>Hello, World!</div>
-    )
+class App extends Component {
+    render() {
+        return ( 
+            <main className="container">
+            <h1 className="text-white text-uppercase text-center my-4">Civilization</h1>
+            <div className="row">
+                <div className="col-md-6 col-sm-10 mx-auto p-0">
+                <div className="card p-3">
+                    This is React example
+                </div>
+                </div>
+            </div>
+            </main>
+        );
+    }
 }
 
 export default App
